e97fffda507cbf059105bd853ad80d39ab026480
Umair Ayub <umairayub79@gmail.com>
2021-08-16 22:58:42 +0500

    first commit

---
+++ utils/savePassword.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const chalk = require('chalk')
+
+const savePassword = (passowrd) => {
+    fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (e, id) => {
+        fs.write(id, passowrd + os.EOL, null, 'utf-8', () => {
+            fs.close(id, () => {
+                console.log(chalk.green('Password saved to passwords.txt'))
+            })
+        })
+    })
+}
+
+module.exports = savePassword
